e7b101aae7c17a84b443a194926148f13e3e93ce
MaaCroX95 <popovic.marko@tutamail.com>
2026-09-14 08:12:04 +0200

    feat: include OpenAI Search gate in settings backup

--- app/src/main/java/com/newoether/agora/data/PortableSettingsArchive.kt
+++ app/src/main/java/com/newoether/agora/data/PortableSettingsArchive.kt
@@ -65,6 +65,7 @@ internal object PortableSettingsArchive {
         put("openAiServiceTierEnabled", JsonPrimitive(sm.openAiServiceTierEnabled.first()))
         put("openAiServiceTier", JsonPrimitive(sm.openAiServiceTier.first()))
         put("openAiResponsesApiEnabled", JsonPrimitive(sm.openAiResponsesApiEnabled.first()))
+        put("openAiWebSearchEnabled", JsonPrimitive(sm.openAiWebSearchEnabled.first()))
         put("anthropicCacheEnabled", JsonPrimitive(sm.anthropicCacheEnabled.first()))
         put("anthropicCacheTtl", JsonPrimitive(sm.anthropicCacheTtl.first()))
         putEncoded("providerBaseUrls", sm.providerBaseUrls.first())
@@ -280,6 +281,9 @@ internal object PortableSettingsArchive {
         }
         obj.boolean("openAiResponsesApiEnabled")?.let {
             sm.saveOpenAiResponsesApiEnabled(it)
+        }
+        obj.boolean("openAiWebSearchEnabled")?.let {
+            sm.saveOpenAiWebSearchEnabled(it)
         }
 
         obj.decode<Map<String, String>>("providerBaseUrls")?.let { imported ->
